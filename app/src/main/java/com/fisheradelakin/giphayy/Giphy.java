2d1c3d92b6c2c1556fc09c97bbc1a471bbceb9cd
Fisher Adelakin <temidayoadelakin@gmail.com>
2015-09-28 21:47:15 -0400

    initial commit // midterms tonight.

---
+++ app/src/main/java/com/fisheradelakin/giphayy/Giphy.java
@@ -0,0 +1,9 @@
+package com.fisheradelakin.giphayy;
+
+/**
+ * Created by Fisher on 9/28/15.
+ */
+public class Giphy {
+
+    public static final String API_URL = "http://api.giphy.com";
+}
---
+++ app/src/main/java/com/fisheradelakin/giphayy/GiphyService.java
@@ -0,0 +1,7 @@
+package com.fisheradelakin.giphayy;
+
+/**
+ * Created by Fisher on 9/28/15.
+ */
+public interface GiphyService {
+}
