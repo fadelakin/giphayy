3dd6805db97f58e9692612719916619b1620e4da
Fisher Adelakin <temidayoadelakin@gmail.com>
2015-09-29 01:09:21 -0400

    back button

--- app/src/main/java/com/fisheradelakin/giphayy/ui/SearchActivity.java
+++ app/src/main/java/com/fisheradelakin/giphayy/ui/SearchActivity.java
@@ -3,6 +3,7 @@ package com.fisheradelakin.giphayy.ui;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -17,7 +18,19 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        toolbar.setTitle("Search");
+
+        // give toolbar back the same behavior as to when the user presses the back button
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+            toolbar.setNavigationIcon(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    NavUtils.navigateUpFromSameTask(SearchActivity.this);
+                    overridePendingTransition(R.anim.pull_in_left, R.anim.push_out_right);
+                }
+            });
+        }
     }
 
     @Override
